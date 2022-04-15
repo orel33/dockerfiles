5e067bdd92136aab7112752e120382328590f69b
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2022-04-15 17:29:22 +0200

    update mkdocs

--- basic/myubuntu/build.sh
+++ basic/myubuntu/build.sh
@@ -4,11 +4,8 @@ set -x
 
 # docker build -t "orel33/myubuntu:latest" .
 # docker push "orel33/myubuntu:latest"
+# docker login registry.gitlab.com # login:orel33
+# docker build -t registry.gitlab.com/orel33/test .
+# docker push registry.gitlab.com/orel33/test
 
-docker login registry.gitlab.com # login:orel33
-
-docker build -t registry.gitlab.com/orel33/test .
-
-docker push registry.gitlab.com/orel33/test
-
-
+docker build -t "orel33/myubuntu:latest" . && docker push "orel33/myubuntu:latest"
