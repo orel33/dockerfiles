d71a8dfa15b10959f0f742270d5319f1f5816392
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2023-09-26 15:54:55 +0200

    add mydebian10

---
+++ basic/mydebian10/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# docker build -t "orel33/mydebian:latest" . && docker push "orel33/mydebian:latest"
+docker build -t "orel33/mydebian10:latest" . && docker push "orel33/mydebian10:latest"
+
