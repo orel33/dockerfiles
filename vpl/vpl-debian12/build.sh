b6e1cc68839978d2362ca672220b6cecab73d89d
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2023-09-27 11:07:16 +0200

    ajout vpl pour debian12

---
+++ vpl/vpl-debian12/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+### build
+OPT="$*" # --no-cache
+echo "OPT=$OPT"
+date > build-date.txt # use to enforce the build of certain layers
+docker build $OPT -t "orel33/vpl-debian12" . || exit 1
+
+### push on docker.com
+# run "docker login" before
+docker push "orel33/vpl-debian12"
+
+### push on docker@ubx
+# run "docker login registry.u-bordeaux.fr"
+docker tag "orel33/vpl-debian12" "registry.u-bordeaux.fr/orel33/vpl-debian12" || exit 1
+docker push "registry.u-bordeaux.fr/orel33/vpl-debian12"
+
+# eof
